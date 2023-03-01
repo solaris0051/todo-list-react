660c3c26965ae1be3243a95dfe14a2c2c80e3366
solaris0051 <30798030+solaris0051@users.noreply.github.com>
2023-03-01 18:31:59 +0900

    +localstorage -visually-hidden

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const DATA = [
-];
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <App tasks={DATA} />
+        <App />
     </React.StrictMode>
 );
